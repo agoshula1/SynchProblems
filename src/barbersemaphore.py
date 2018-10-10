e7baa0b09f1b3f4117fc77aa5896e7bda9280a82
agoshula <agoshula@uvic.ca>
2018-10-09 20:36:53 -0700

    reorganizing and testing code for barbershop problem

--- src/barbersemaphore.py
+++ src/barbersemaphore.py
@@ -4,15 +4,18 @@
  As in the book, this solution uses semaphores as the main concurrency mechanism.
 '''
 import threading
+import time
 
 class SharedData():
     def __init__(self):
         self.chairs = 4
         self.customers = 0
+        self.stopBarber = False
 
 def customer(data,n):
     mutex.acquire()
-    if data.customers == data.chairs:
+    if data.customers == data.chairs or data.stopBarber:
+        #print "Customer " + str(n) + " rejected\n"
         mutex.release()
         return #leave the barbershop
     data.customers += 1
@@ -23,7 +26,7 @@ def customer(data,n):
     barb.acquire()
 
     #get haircut
-    print "Customer " + str(n)
+    #print "Customer " + str(n) + " getting hair cut\n"
 
     #rendezvous
     customerDone.release()
@@ -33,17 +36,74 @@ def customer(data,n):
     data.customers -= 1
     mutex.release()
 
-def barber(n):
-    #rendezvous
-    cust.acquire()
-    barb.release()
+def lastCustomer(data,n):
+    #print "last customer"
+    mutex.acquire()
+    if data.customers == data.chairs:
+        data.stopBarber = True #premptively stop barber
+        #print "Customer " + str(n) + " rejected\n"
+        mutex.release()
+        return #leave the barbershop
+    data.customers += 1
+    mutex.release()
 
-    #cut hair
-    print "Barber " + str(n)
+    #rendezvous
+    cust.release()
+    barb.acquire()
+
+    #get haircut
+    #print "Customer " + str(n) + " getting hair cut\n"
 
     #rendezvous
-    customerDone.acquire()
-    barberDone.release()
+    customerDone.release()
+    barberDone.acquire()
+
+    mutex.acquire()
+    data.customers -= 1
+    data.stopBarber = True #premptively stop barber
+    mutex.release()
+
+#runs indefinitely unless forced to stop
+def barber(data):
+    while(True):
+        #rendezvous
+        time.sleep(0.01) #give last customer time to communicate finish
+        if data.stopBarber:
+            break
+        cust.acquire()
+        barb.release()
+
+        #cut hair
+        #print "Barber cutting hair\n"
+
+        #rendezvous
+        customerDone.acquire()
+        barberDone.release()
+
+def simulate(numCusts,data):
+    threads = []
+    r = threading.Thread(target=barber, args=(data,))
+    r.start()
+
+    for n in range(numCusts - 1):
+        t = threading.Thread(target=customer, args=(data,n))
+        threads.append(t)
+        t.start()
+        #time.sleep(0.01)
+
+    for i in range(len(threads)):
+        threads[i].join()
+        if threads[i].isAlive():
+            print "Customer " + str(i) + " still running"
+
+    #last customer, may stop barber before all seated customers are given haircut
+    t = threading.Thread(target=lastCustomer, args=(data,numCusts - 1))
+    t.start()
+    t.join()
+
+    #print "customers done"
+    r.join()
+    #print "barber done"
 
 shareddata = SharedData()
 mutex = threading.Semaphore(1)
@@ -51,26 +111,21 @@ cust = threading.Semaphore(0)
 barb = threading.Semaphore(0)
 customerDone = threading.Semaphore (0)
 barberDone = threading.Semaphore (0)
-ncustthreads = 10
 
-#need to find how to keep a barber around while customers are present
-threads = []
-for n in range(ncustthreads):
-    t = threading.Thread(target=customer, args=(shareddata,n))
-    threads.append(t)
-    t.start()
-    r = threading.Thread(target=barber, args=(n,))
-    threads.append(r)
-    r.start()
-for i in range(len(threads)):
-    threads[i].join()
-'''while(True):
-    t = threading.Thread(target=barber, args=(n,))
-    t.start()
-    deadcnt = 0
-    for n in range(ncustthreads):
-        if not threads[n].isAlive():
-            deadcnt += 1
-    if deadcnt == ncustthreads:
-        break
-'''
+#correctness testing
+#simulate(30,shareddata)
+
+#performance testing
+t0 = time.clock()
+simulate(20,shareddata);
+print "Step 1: Time elapsed (sec) = {}".format(time.clock() - t0)
+
+shareddata = SharedData()
+t0 = time.clock()
+simulate(200,shareddata);
+print "Step 2: Time elapsed (sec) = {}".format(time.clock() - t0)
+
+shareddata = SharedData()
+t0 = time.clock()
+simulate(2000,shareddata);
+print "Step 3: Time elapsed (sec) = {}".format(time.clock() - t0)
--- src/barbershopchan.go
+++ src/barbershopchan.go
@@ -8,6 +8,7 @@ package main
 import (
     "fmt"
     "time"
+    "sync"
 )
 
 type Customer struct{
@@ -20,37 +21,67 @@ type Barbershop struct{
   BarbershopCh chan Customer
 }
 
+func simulate(numCusts int){
+    b := Barbershop{4, make(chan bool), make(chan Customer)}
+    barberCh := make(chan Customer, 4) //waiting room seats
+    var wait sync.WaitGroup
+    wait.Add(1)
+
+    //barber
+    go func(){
+      for len(barberCh) > 0{
+        //c := <- barberCh
+        <- barberCh
+        //cut hair
+        //fmt.Printf("Cutting hair of customer %d\n", c.id)
+        time.Sleep(10000000) //0.01 seconds
+      }
+      wait.Done()
+    }()
+    //customers
+    go func(){
+      for i := 0; i < numCusts; i++{
+        b.BarbershopCh <- Customer{i}
+        <- b.WaitingCh
+      }
+    }()
+    for i := 0; i < numCusts; i++{
+      c := <- b.BarbershopCh
+      //fmt.Printf("Number of taken chairs: %d\n", len(barberCh))
+      switch{
+      case len(barberCh) == b.WaitRoomSize:
+        //reject customer from waiting room
+        //fmt.Printf("Customer %d rejected\n", c.id)
+        b.WaitingCh <- false
+      default:
+        //allow customer into waiting room
+        barberCh <- c
+        //fmt.Printf("Customer %d waiting\n", c.id)
+        b.WaitingCh <- true
+      }
+    }
+    wait.Wait()
+}
 func main(){
-  b := Barbershop{4, make(chan bool), make(chan Customer)}
-  barberCh := make(chan Customer, 4) //waiting room seats
-  //barber
-  go func(){
-    for{
-      <- barberCh
-      //cut hair
-      time.Sleep(1)
-    }
-  }()
-  //customer
-  go func(){
-    i := 0
-    for{
-      b.BarbershopCh <- Customer{i}
-      <- b.WaitingCh
-      i++
-    }
-  }()
-  for{
-    c := <- b.BarbershopCh
-    switch{
-    case len(barberCh) == b.WaitRoomSize:
-      //reject customer from waiting room
-      b.WaitingCh <- false
-    default:
-      //allow customer into waiting room
-      barberCh <- c
-      fmt.Printf("Customer %d waiting", c.id)
-      b.WaitingCh <- true
-    }
-  }
+  //correctness testing
+  //simulate(30)
+
+  //performance testing
+  start := time.Now()
+  simulate(20)
+  end := time.Now()
+  difference := end.Sub(start)
+  fmt.Printf("Step 1: Time elapsed (sec) = %f\n", difference.Seconds())
+
+  start = time.Now()
+  simulate(200)
+  end = time.Now()
+  difference = end.Sub(start)
+  fmt.Printf("Step 2: Time elapsed (sec) = %f\n", difference.Seconds())
+
+  start = time.Now()
+  simulate(2000)
+  end = time.Now()
+  difference = end.Sub(start)
+  fmt.Printf("Step 3: Time elapsed (sec) = %f\n", difference.Seconds())
 }
